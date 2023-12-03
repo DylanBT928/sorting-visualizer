6174c8a475cb2c362137978c15635a1bd05b85ef
Dylan Tran <dylanbt928@gmail.com>
2023-12-03 15:16:36 -0800

    Added debug and release folders

--- Algorithms/test.cpp
+++ Algorithms/test.cpp
@@ -1,24 +1,43 @@
 #include <SFML/Graphics.hpp>
+using namespace sf;
+
+struct data
+{
+	int height;
+	int xpos;
+} line[100];
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+	RenderWindow window;
+	window.create(VideoMode(800, 500), "Testing");
+	window.setPosition(Vector2i(500, 200));
+	
+	RectangleShape rectangle(Vector2f(120.f, 50.f));
+	rectangle.setFillColor(Color::White);
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+	while (window.isOpen())
+	{
+		Event event;
+		window.clear(Color::Black);
+		srand(time(NULL));
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				window.close();
+		}
 
-    return 0;
+		for (int i = 0; i < 100; i++)
+		{
+			rectangle.setFillColor(Color::White);
+			rectangle.setPosition(8, 500);
+			rectangle.setSize(Vector2f(8, 100)); //TODO: randomize the 100
+			rectangle.setRotation(180);
+			window.draw(rectangle);
+		}
+		window.display();
+	}
+
+	return 0;
 }
