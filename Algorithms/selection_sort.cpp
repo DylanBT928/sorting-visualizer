6bc0bb43b0e4dbbe0d664183327efaf0bc18c26f
Dylan Tran <dylanbt928@gmail.com>
2023-12-08 20:54:27 -0800

    Checked if values are sorted
    - Organized code for better functionality
    - Switch statement for events

--- Algorithms/selection_sort.cpp
+++ Algorithms/selection_sort.cpp
@@ -1,47 +1,66 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
+
 using namespace sf;
 
-struct data
+RenderWindow window;
+RectangleShape rectangle(Vector2f(120.f, 50.f));
+std::vector<int> values;
+struct data { int height, xpos; } line[100];
+
+bool isSorted(std::vector<int> val)
 {
-	int height;
-	int xpos;
-} line[100];
+	for (int i = 0; i < val.size()-1; i++)
+	{
+		for (int j = 1; j < val.size(); j++)
+		{
+			if (val[i] > val[j])
+			{
+				return false;
+			}
+		}
+	}
+	return true;
+}
 
 int main()
 {
-	RenderWindow window;
-	window.create(VideoMode(800, 500), "Selection Sort");
-	window.setPosition(Vector2i(500, 200));
-	
-	RectangleShape rectangle(Vector2f(120.f, 50.f));
-	rectangle.setFillColor(Color::White);
-	std::vector<int> values;
-	srand(time(NULL));
+    window.create(VideoMode(800, 500), "Selection Sort");
+    window.setPosition(Vector2i(400, 200));
 
-	while (window.isOpen())
-	{
-		Event event;
-		window.clear(Color::Black);
-		srand(time(NULL));
+    rectangle.setFillColor(Color::White);
+    srand(time(NULL));
 
-		while (window.pollEvent(event))
-		{
-			if (event.type == Event::Closed)
-				window.close();
-		}
+    while (window.isOpen())
+    {
+        Event event;
+        window.clear(Color::Black);
+        srand(time(NULL));
 
-		for (int i = 0; i < 100; i++)
-		{
-			values.push_back(rand()%45+1);
-			rectangle.setFillColor(Color::White);
-			rectangle.setPosition(8+8*i, 500);
-			rectangle.setSize(Vector2f(8, values[i]*10));
-			rectangle.setRotation(180);
-			window.draw(rectangle);
-		}
-		window.display();
-	}
+        while (window.pollEvent(event))
+        {
+            switch (event.type)
+            {
+                case Event::Closed:
+                    window.close();
+                    break;
+            }
+        }
 
-	return 0;
+        // Creates rectangles with randomized heights
+        // Heights are stored in "values" vector
+        for (int i = 0; i < 100; i++)
+        {
+            values.push_back(rand()%45+1);
+            rectangle.setFillColor(Color::White);
+            rectangle.setPosition(8+8*i, 500);
+            rectangle.setSize(Vector2f(8, values[i]*10));
+            rectangle.setRotation(180);
+            window.draw(rectangle);
+        }
+
+        window.display();
+    }
+
+    return 0;
 }
